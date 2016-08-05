8f614f87d4371c747872fb54bfc12f07e208ce93
Jeremy Zornow <jeremy@zornow.com>
2016-08-04 20:11:19 -0600

    List can now be filled. Also: List#length.

--- lib/list.rb
+++ lib/list.rb
@@ -2,10 +2,21 @@ require 'link'
 
 class List
   def initialize(*items)
-    @head = nil
+    @head = cursor = nil
+
+    items.reverse.each do |item|
+      cursor = Link.new(item, cursor)
+      @head = cursor
+    end
   end
 
   def length
-    0
+    cursor = @head
+    sum = 0
+    until cursor.nil?
+      sum += 1
+      cursor = cursor.rest
+    end
+    sum
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe '#length' do
       expect(list.length).to eq 0
     end
   end
+  context 'with a positive number of links' do
+    it 'returns the number of links' do
+      list = List.new 1, 2, 3, 4, 5
+      expect(list.length).to eq 5
+    end
+  end
 end
